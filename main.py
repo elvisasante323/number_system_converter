c76b7b0738aa41c6c2bd77ccc121756d20202e8b
Elvis  Asante <elvisasante5@yahoo.com>
2022-10-07 18:56:31 +0100

    Introduction
    Wrote comments that introduces the purpose of the program

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+ Being inspired by lessons in CS Mathematics, I decided to write programs for certain mathematical computations. This
+ program converts numbers from one numerical system to another. The following systems are supported :
+
+ - Binary to decimal
+ - Hexadecimal to decimal
+ - Octal to decimal
+"""
